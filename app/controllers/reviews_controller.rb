b73ea82a5545ecc7de011f7e64e0a84b49175470
RickLearnsToCode <rick.rosetti@gmail.com>
2020-02-04 08:07:19 -0700

    Add select dropdown to create_review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,10 @@
 class ReviewsController < ApplicationController
     get '/reviews/new' do
+        # binding.pry
         if !!params[:beer_id]
             @beer = Beer.find_by_id(params[:beer_id])
         end
-        erb :'reviews/new'
+        erb :'reviews/create_review'
 
     end
 
--- config.ru
+++ config.ru
@@ -7,4 +7,5 @@ end
 use Rack::MethodOverride
 use UsersController
 use BeersController
+use ReviewsController
 run ApplicationController
